6557b73388b3f1cdc9570dfe7069c02371fb811c
BigSteveXD <24504153+BigSteveXD@users.noreply.github.com>
2025-07-09 09:57:13 -0700

    Ch2 L1 add middleware wrapper to count website hits

--- main.go
+++ main.go
@@ -3,13 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) countHits(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	//new http.ServeMux
 	myServeMux := http.NewServeMux()
 
-	myServeMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiCfg := &apiConfig{}
+
+	myServeMux.Handle("/app/", apiCfg.middlewareMetricsInc( http.StripPrefix("/app", http.FileServer(http.Dir("."))) ))//fileserver is a handler
+
+	myServeMux.Handle("/metrics", http.HandlerFunc(apiCfg.countHits))//handle the wrap(handler)
+	myServeMux.Handle("/reset", http.HandlerFunc(apiCfg.resetHits))
 
 	//readiness endpoint
 	myServeMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
